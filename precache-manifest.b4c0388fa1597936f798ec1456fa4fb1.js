b76afbf3febec83ff41e8a8bf0556f6f8c7a8218
MariangelMM <mariangelmoramarquez@gmail.com>
2020-04-08 17:30:19 -0500

    Updates

---
+++ precache-manifest.b4c0388fa1597936f798ec1456fa4fb1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4517a822128e9a5b853480d2c268957a",
+    "url": "/LIM011-BURGERQUEEN/index.html"
+  },
+  {
+    "revision": "28fa6406af27e6824a56",
+    "url": "/LIM011-BURGERQUEEN/static/css/main.f17b9be7.chunk.css"
+  },
+  {
+    "revision": "234bded954067ad3a290",
+    "url": "/LIM011-BURGERQUEEN/static/js/2.d2829557.chunk.js"
+  },
+  {
+    "revision": "07a707eba59e3e3ed782f77b31f34780",
+    "url": "/LIM011-BURGERQUEEN/static/js/2.d2829557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28fa6406af27e6824a56",
+    "url": "/LIM011-BURGERQUEEN/static/js/main.a2229967.chunk.js"
+  },
+  {
+    "revision": "e4e6e0b0fec3939c65b3",
+    "url": "/LIM011-BURGERQUEEN/static/js/runtime-main.d3065a67.js"
+  },
+  {
+    "revision": "9829bde7dca904dc4b7f1e7684dabf4e",
+    "url": "/LIM011-BURGERQUEEN/static/media/fondoBurger.9829bde7.svg"
+  },
+  {
+    "revision": "815dae7ee10ffd1d6f9039c2c4309ca3",
+    "url": "/LIM011-BURGERQUEEN/static/media/fondoHeader.815dae7e.svg"
+  },
+  {
+    "revision": "41f17378fa14d06793f8263773e3e426",
+    "url": "/LIM011-BURGERQUEEN/static/media/logoReina.41f17378.svg"
+  }
+]);
